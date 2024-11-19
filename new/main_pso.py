93dd0bcd60115e341009b2c63c6aa440d1a00585
solesonnino <soleester.sonnino@gmail.com>
2024-11-19 20:17:25 +0100

    pso implementation

--- new/main_pso.py
+++ new/main_pso.py
@@ -53,9 +53,50 @@ def main():
         fitness_Vec= np.array(fitness)
         print(f"the fitness values are: {fitness_Vec} \n")
 
+
         # Receive the variable 'trigger_end' from C# code
         trigger_end = int(s.recv(1024).decode())
         print(f"Trigger end: {trigger_end}")
+
+        #update the particles positions 
+
+        #set pbest and gbest
+        if trigger_end ==1 : #only  at the firts iteration
+            # best personal position of each particle
+            personal_best_positions = particle_positions.copy()
+            personal_best_scores =fitness_Vec.copy()
+
+            # best (initial) global best position
+            global_best_position = personal_best_positions[np.argmin(personal_best_scores)]
+            global_best_score = np.min(personal_best_scores)
+
+        else :
+            for i in range (num_particles):
+                current_fitting_value = fitness_Vec [i]
+
+                 # update the personal best if it is necessary
+                if current_fitting_value < personal_best_scores[i]:
+                    personal_best_positions[i] = particle_positions[i]
+                    personal_best_scores[i] = current_fitting_value
+                
+                # update the global best if necessary
+                if current_fitting_value < global_best_score:
+                    global_best_position = particle_positions[i]
+                    global_best_score = current_fitting_value
+        #update particles            
+        for i in range (num_particles):
+            # update the velocity according to the formula
+            inertia = inertia_weight * particle_velocities[i]
+            cognitive = cognitive_component * np.random.random() * (personal_best_positions[i] - particle_positions[i])
+            social = social_component * np.random.random() * (global_best_position - particle_positions[i])
+            particle_velocities[i] = inertia + cognitive + social
+                
+            # update the position of the particle
+            particle_positions[i] += particle_velocities[i]
+            particle_positions[i] = int(particle_positions[i])  # conversione a intero
+    print (f"global best position: {global_best_position}") 
+    print (f"global best score: {global_best_score}")  
+
 
     # Close the connection
     s.close()
