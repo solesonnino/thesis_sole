26071b3b12380c63a5d6c42621d9ba603d29a5f0
solesonnino <soleester.sonnino@gmail.com>
2025-02-01 20:19:49 +0100

    setup test 3dbinpacking

---
+++ final/test_packing.py
@@ -0,0 +1,47 @@
+from place_visualize_obj import Scene, Packer, Bin, Item
+
+packers=[]
+
+packer1 = Packer()
+packer2=Packer()
+packers.append(packer1)
+packers.append(packer2)
+packer1.add_bin(Bin('Type1_box', 76, 90, 100, 20))
+packer2.add_bin(Bin('Type2_box', 150, 200, 200, 20))
+packer1.add_item(Item('Type1', 25,25,25, 1))
+packer1.add_item(Item('Type1', 25,25,25, 1))
+packer1.add_item(Item('Tipe1', 25,25,25, 1))
+packer1.add_item(Item('Tipe1', 25,25,25, 1))
+packer2.add_item(Item('Type2', 25,25,25, 1))
+packer2.add_item(Item('Type2', 25,25,25, 1))
+packer2.add_item(Item('Type2', 25,25,25, 1))
+packer2.add_item(Item('Type2', 25,25,25, 1))
+packer2.add_item(Item('Type2', 25,25,25, 1))
+packer2.add_item(Item('Type2', 25,25,25, 1))
+
+
+items=[] 
+
+
+for packer in packers:
+    packer.pack()
+    for b in packer.bins:
+            scene = Scene()
+            print(":::::::::::", b.string())
+            scene.add_object_to_scene(b, False)
+            print("FITTED ITEMS:")
+            place_points=[]
+            
+            for item in b.items:
+                print("====> ", item.string())
+                scene.add_object_to_scene(item, False)
+                print(item.get_center())
+                items.append(item) #store into an array all the items to be picked and placed
+            
+            print("UNFITTED ITEMS:")
+            for item in b.unfitted_items:
+                print("====> ", item.string())
+
+            print("***************************************************")
+            print("***************************************************")
+            scene.show_scene()
